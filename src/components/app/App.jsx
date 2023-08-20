805cf1467a4c33800aeb6494314553af8984c003
Victor Stupitsyn <st.viktorr@gmail.com>
2023-08-20 21:59:37 +0300

    добавил Layout Outlet

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,27 +1,26 @@
 import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "../layout/header/header";
-import Footer from "../layout/footer/footer";
 import About from "../pages/about/about";
 import Portfolio from "../pages/portfolio/portfolio";
 import Experience from "../pages/experience/experience";
 import { GlobalStyle } from "./styles";
+import Layout from "../layout/layout";
 
 function App({ portfolioData }) {
   return (
     <>
       <GlobalStyle />
       <Router>
-        <Header />
         <Routes>
-          <Route path="/" element={<About />}></Route>
-          <Route
-            path="/portfolio"
-            element={<Portfolio portfolioData={portfolioData} />}
-          ></Route>
-          <Route path="/experience" element={<Experience />}></Route>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<About />}></Route>
+            <Route
+              path="/portfolio"
+              element={<Portfolio portfolioData={portfolioData} />}
+            ></Route>
+            <Route path="/experience" element={<Experience />}></Route>
+          </Route>
         </Routes>
-        <Footer />
       </Router>
     </>
   );
